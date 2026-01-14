069288d216749f4be8778367fbe4e4d72ae54808
boudy <abdulraman.ahmedd@gmail.com>
2026-01-14 15:19:38 +0200

    create token model

---
+++ exam-system/src/main/java/com/exam/exam_system/Entities/Token.java
@@ -0,0 +1,5 @@
+package com.exam.exam_system.Entities;
+
+public class Token {
+
+}
---
+++ exam-system/src/main/java/com/exam/exam_system/repository/TokenRepository.java
@@ -0,0 +1,19 @@
+package com.exam.exam_system.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.exam.exam_system.Entities.Token;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Long> {
+    Optional<Token> findByToken(String token);
+    @Query("""
+    	    SELECT t FROM Token t WHERE t.user.id = :userId AND t.expired = false AND t.revoked = false
+    	""")
+    	List<Token> findAllValidTokenByUser(Long userId);
+}
